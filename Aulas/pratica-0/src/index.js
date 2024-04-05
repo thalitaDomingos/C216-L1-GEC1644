5f1c48683ccd39904cd422a65cd1bf77bd033c15
thalitaDomingos <90002483+thalitaDomingos@users.noreply.github.com>
2024-04-05 18:18:36 -0300

    Repositorio C216-L1

---
+++ Aulas/pratica-0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = "Thalita Domingos - 1644";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ Aulas/pratica-0/src/module.js
@@ -0,0 +1,7 @@
+// arquivo modulo.js
+module.exports = {
+  mensagem: 'Testando modulo: Thalita Domingos - 1644!',
+  saudacao: function() {
+    console.log(this.mensagem);
+  }
+};
